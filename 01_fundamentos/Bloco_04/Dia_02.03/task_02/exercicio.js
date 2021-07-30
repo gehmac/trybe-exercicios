da667537a0886ed89906655c47cae7d49f4ed750
Ge <gmarcoscardoso@gmail.com>
2021-07-30 14:39:12 -0300

    task

---
+++ 01_fundamentos/Bloco_04/Dia_02.03/task_02/exercicio.js
@@ -0,0 +1,39 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, -2, 150];
+// 1- Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// for(let index = 0; index < numbers.length; index += 1){
+//     console.log(numbers[index])
+// }
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// let soma = 0;
+// for(let index = 0; index < numbers.length; index += 1){
+//      soma = soma + numbers[index]
+// }
+// console.log(soma)
+
+//  Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array; 
+// let soma = 0;
+// for(let index = 0; index < numbers.length; index += 1){
+//     soma = soma + numbers[index]
+// }
+// console.log(soma / numbers.length)
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// let soma = 0;
+// for(let index = 0; index < numbers.length; index += 1){
+//     soma = soma + numbers[index]
+// }
+// let media = soma / numbers.length
+// if( media > 20 )
+//     console.log("Valor maior que 20")
+//  else 
+//     console.log("valor menor ou igual a 20")
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o; 
+let maior = 0
+for(let index = 0; index < numbers.length; index += 1){
+    if (numbers[index] > maior){
+        maior = numbers[index]
+    }
+}
+console.log(maior)
