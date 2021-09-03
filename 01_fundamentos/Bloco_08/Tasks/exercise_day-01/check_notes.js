dfeec5cf74729fee6edb72a042c67e4fd567defb
Ge <gmarcoscardoso@gmail.com>
2021-09-03 11:14:33 -0300

    check notes

--- 01_fundamentos/Bloco_08/Tasks/exercise_day-01/check_notes.js
+++ 01_fundamentos/Bloco_08/Tasks/exercise_day-01/check_notes.js
@@ -1,13 +1,12 @@
 /* 
   ? Cria uma função que compara a nota dos alunas
-  ! depois pensa no app
 */
 
 const right_answer = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const student_answers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const checkNotes = (right, student, action) => {
-  let count = 0
+  let count = 0; // coloca 0 sempre que tiver uma soma
   for (let index = 0; index < right.length; index += 1){
     const countAction = action(right[index], student[index]);
     count += countAction
