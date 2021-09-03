b5df187b6885ece9e9d95b282267fe7c78661da1
Ge <gmarcoscardoso@gmail.com>
2021-09-03 11:10:00 -0300

    check_notes

---
+++ 01_fundamentos/Bloco_08/Tasks/exercise_day-01/check_notes.js
@@ -0,0 +1,24 @@
+/* 
+  ? Cria uma função que compara a nota dos alunas
+  ! depois pensa no app
+*/
+
+const right_answer = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const student_answers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkNotes = (right, student, action) => {
+  let count = 0
+  for (let index = 0; index < right.length; index += 1){
+    const countAction = action(right[index], student[index]);
+    count += countAction
+  }
+  return count
+}
+
+const comparisonNumber = (parameterRight, parameterStudent) => {
+  if (parameterRight === parameterStudent) return 1 
+  return 0.5;
+}
+
+
+console.log(checkNotes(right_answer, student_answers, comparisonNumber))
