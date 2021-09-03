12750beb337dc7665465fe10e9784bee13a273ed
Ge <gmarcoscardoso@gmail.com>
2021-09-03 07:45:39 -0300

    add comments

--- 01_fundamentos/Bloco_08/Tasks/exercise/task_01.js
+++ 01_fundamentos/Bloco_08/Tasks/exercise/task_01.js
@@ -1,7 +1,12 @@
-const newEmployees = (callBack) => {
+/* 
+  Uma função que retorna outra função com um obj, 
+  e executa com os parametros da segunda função
+*/
+
+const newEmployees = (callBack) => {  // recebe a função (o nome nao precisa ser o mesmo da função)
   const employees = {
-    id1: callBack('Pedro Guerra'), 
-    id2: callBack('Luiza Drumond'), 
+    id1: callBack('Pedro Guerra'),  // passs como parametro o nome das pessoas 
+    id2: callBack('Luiza Drumond'), // funcionaria da mesma forma com o recebimento de um banco de dados
     id3: callBack('Carla Paiva'), 
   }
   return employees;
@@ -9,9 +14,11 @@ const newEmployees = (callBack) => {
 
 const creatEmail = (fullName) => {
   return fullName.replace(/\s/g, '_');
+  /* a primeira parte tira os espaços
+  a segunda parte eu coloco pelo que vai substituir */
 }
 
-const creatPerson = (fullName) => ({
+const creatPerson = (fullName) => ({ // essa e a regra do negócio
   fullName,
   email: creatEmail(`${fullName}@trybe.com`)
 })
