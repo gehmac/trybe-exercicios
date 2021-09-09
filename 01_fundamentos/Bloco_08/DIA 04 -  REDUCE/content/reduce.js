54266f26feb86a942f0a0f6750acac982c3e23c6
Ge <gmarcoscardoso@gmail.com>
2021-09-09 16:17:23 -0300

    exemplo 3.1

--- 01_fundamentos/Bloco_08/DIA 04 -  REDUCE/content/reduce.js
+++ 01_fundamentos/Bloco_08/DIA 04 -  REDUCE/content/reduce.js
@@ -10,8 +10,11 @@ const numbers = [1, 2, 2, 10, -2];
 const getSum = (result, number) => {
 console.log(`result: ${result}  +  number: ${number}.`);
 return result + number;}
-const sumNumbers = numbers.reduce(getSum);
+const sumNumbers = numbers.reduce(getSum, 0);
 console.table(" o resultado é " + sumNumbers); 
+
+
+
 
 /* 
  * EXEMPLO 02
@@ -26,3 +29,35 @@ const getBigger = (bigger, number) => {
 }
 const bigger = numbersEx2.reduce(getBigger, 0);
 console.log(" o resultado é " + bigger); // 85
+
+
+
+/* 
+ * EXEMPLO 03
+  - some o numeros pares
+*/console.log(`\n  EXEMPLO 03`);
+
+const numbersOfEven = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0
+const sumpPair = (currentValue, number) => {
+console.log(`${currentValue} - ${number}`);
+return currentValue + number}
+
+const sumNumber1 = (array) => array.filter(getEven).reduce(sumpPair)
+
+console.log(" o resultado é " + sumNumber1(numbersOfEven));
+
+
+/* 
+ * EXEMPLO 03.1
+  - some o numeros pares
+*/console.log(`\n  EXEMPLO 03.1`);
+
+const sumpPair2 = (currentValue, number) => {
+console.log(`${currentValue} - ${number}`);
+return (number % 2 === 0) ?  currentValue + number : currentValue}
+
+const sumNumber2 = (array) => array.reduce(sumpPair2, 0)
+
+console.log(" o resultado é " + sumNumber2(numbersOfEven));
