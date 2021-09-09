0cb77add9181fe0fa949ffe3619784fc94165265
Ge <gmarcoscardoso@gmail.com>
2021-09-09 15:42:53 -0300

    exmplo 2

--- 01_fundamentos/Bloco_08/DIA 04 -  REDUCE/content/reduce.js
+++ 01_fundamentos/Bloco_08/DIA 04 -  REDUCE/content/reduce.js
@@ -6,12 +6,23 @@
   - soma os numeros
 */console.log(`\n  EXEMPLO 01`);
 
-const numbers = [32, 15, 3, 2, -5, 56, 10];
-const getSum = (result, number) => result + number;
+const numbers = [1, 2, 2, 10, -2];
+const getSum = (result, number) => {
+console.log(`result: ${result}  +  number: ${number}.`);
+return result + number;}
 const sumNumbers = numbers.reduce(getSum);
-console.log("o resultado é " + sumNumbers); 
+console.table(" o resultado é " + sumNumbers); 
 
 /* 
  * EXEMPLO 02
-  - transforma os numeros em negativos
+  - encontra o maior numero
 */console.log(`\n  EXEMPLO 02`);
+
+const numbersEx2 = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => {
+  console.log(`bigger: ${bigger} x number: ${number}`);
+  return (bigger > number) ? bigger : number;
+}
+const bigger = numbersEx2.reduce(getBigger, 0);
+console.log(" o resultado é " + bigger); // 85
