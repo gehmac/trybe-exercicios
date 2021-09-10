ef09fb0fa9f16e9544fa5318159740e3928d76fc
Ge <gmarcoscardoso@gmail.com>
2021-09-10 08:55:34 -0300

    manipulating book array

---
+++ 01_fundamentos/Bloco_08/DIA 04 -  REDUCE/tasks/manipulating_book_array.js
@@ -0,0 +1,109 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+
+//  * nomes de todas as pessoas autoras
+
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+const getArray = (acc, nameArr) => `${acc}, ${nameArr}`
+const getDot = (acc, nameArr) => `${acc}, ${nameArr}.`
+const reduceNames = () => books.map((book) => book.author.name)
+    .reduce((acc, nameArr, index, array) => (index === array.length -1) ? 
+      getDot(acc, nameArr) : getArray(acc, nameArr) )
+
+
+// * calcule a media das idades
+
+const expectedAge = 43;
+
+function averageAge() {
+  const getMedia = (acc, age) => acc + age 
+  const arrayAge =  books.map((book) => book.releaseYear - book.author.birthYear ).reduce(getMedia)
+  return arrayAge / books.length
+}
+
+// * Livro com o maior nome
+
+const expectedName = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+}
+
+function longestNamedBook() {
+  const getBook = (accBook, currentbook, ) => (accBook.name.length > currentbook.name.length) ? accBook : currentbook  
+  return books.reduce(getBook)
+}
+
+
+assert.strictEqual(reduceNames(), expectedResult);
+assert.strictEqual(averageAge(), expectedAge);
+assert.deepStrictEqual(longestNamedBook(), expectedName);
