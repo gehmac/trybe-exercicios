56c4bc3dba53d92d8086838a05d7638e165ee989
Ge <gmarcoscardoso@gmail.com>
2021-09-16 13:16:13 -0300

    constents: asychronous

--- 01_fundamentos/Bloco_09/contents/Asynchronous_operations.js
+++ 01_fundamentos/Bloco_09/contents/Asynchronous_operations.js
@@ -10,40 +10,8 @@ const pushNumber = (list, number) => {
 
 let numbers = [];
 
-setTimeout(() => pushNumber(numbers, 1), 5000);
+setTimeout(() => pushNumber(numbers, 1), 3000);
 pushNumber(numbers, 2);
 pushNumber(numbers, 3);
 
 setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 5000);// se for menor que 5000 vai falar
-
-// ! ----------------------------------------------------
-
-const countryName = ({ name }) => console.log(`Returned country is ${name}`);
-const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
-
-const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
-
-const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
-
-const getCountry = (onSuccess) => {
-  setTimeout(() => {
-    const didOperationSucceed = Math.random() >= 0.5;
-    if(didOperationSucceed) {
-      const country = {
-        name: "Brazil",
-        hdi: 0.759,
-        currency: "Real",
-      };
-      onSuccess(country);
-    }
-    else {
-      const errorMessage = "Country could not be found";
-    }
-  }, delay());
-};
-
-// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
-getCountry(countryName, printErrorMessage);
-
-// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
-getCountry(countryCurrency, printErrorMessage);
