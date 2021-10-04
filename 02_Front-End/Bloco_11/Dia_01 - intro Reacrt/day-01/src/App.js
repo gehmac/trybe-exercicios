f38c64a8079f803929a6e596599d3ac48cb8ae38
Ge <gmarcoscardoso@gmail.com>
2021-10-04 20:56:16 -0300

    My first React Class Component

---
+++ 02_Front-End/Bloco_11/Dia_01 - intro Reacrt/day-01/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './App.css';
+
+class ReactClass extends Component {
+    render() {
+      return (
+        <h1>My first React Class Component!</h1>
+      )
+    }
+  }
+
+export default ReactClass;
